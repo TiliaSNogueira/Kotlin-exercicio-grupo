bbd00587e483341505b5e2f4e8dec2c66c7198fa
MiguelFazzanaro <mfazzanaro@gmail.com>
2020-09-15 19:02:25 -0300

    feito merge dos branch e finalizado o exercicio

--- src/Estoque/Main.kt
+++ src/Estoque/Main.kt
@@ -6,7 +6,7 @@ fun main() {
     var cadastro3 = Estoque("Item3", -200,-1)
 
     cadastro1.mudarNome("IIItem1")
-    cadastro2.mudarQtdMinima(5)
+    cadastro2.mudarQuantidadeMinima(5)
     cadastro2.repor(50)
     cadastro1.darBaixa(95)
     cadastro1.precisaRepor()
